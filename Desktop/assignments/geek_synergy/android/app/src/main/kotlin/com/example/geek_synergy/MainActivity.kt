1dd2d42f1d488a4fbd65bf4c91ce3cb23cfd6217
Priyanka Arora <77575440+PriyankaArora09@users.noreply.github.com>
2024-06-26 20:10:20 +0530

    Delete Desktop/assignments/geek_synergy directory

--- Desktop/assignments/geek_synergy/android/app/src/main/kotlin/com/example/geek_synergy/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.geek_synergy
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Desktop/assignments/geek_synergy/lib/theme/app_paddings.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-class AppPadding {
-  // static EdgeInsets registerScreenPadding =
-  //     EdgeInsets.symmetric(vertical: 30.h, horizontal: 20.w);
-  static EdgeInsets mainPadding =
-      EdgeInsets.symmetric(vertical: 30.h, horizontal: 20.w);
-}
