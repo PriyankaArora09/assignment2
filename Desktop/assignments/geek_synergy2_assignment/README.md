bd289339ed375cbe3324a064e488c8ef72a3da37
Priyanka <arorapriyanka1801@gmail.com>
2024-06-26 20:08:19 +0530

    Initial commit with correct line endings

---
+++ Desktop/assignments/geek_synergy2_assignment/README.md
@@ -0,0 +1,3 @@
+# geek_synergy2_assignment
+
+A new Flutter project.
---
+++ Desktop/assignments/geek_synergy2_assignment/android/app/src/main/kotlin/com/example/geek_synergy2_assignment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.geek_synergy2_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
